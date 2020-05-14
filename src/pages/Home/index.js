94663a562f748e7ccb2b9633df0422f1b9ffe418
Victor Fontenele <victorvitoria11@hotmail.com>
2020-05-13 21:06:24 -0300

    feat: initial settings - routes, pages, components, styles

---
+++ src/pages/Home/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+
+// import { Container } from './styles';
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <h1>Homepage</h1>
+    </>
+  );
+}
+
+export default Home;
