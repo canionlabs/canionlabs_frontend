0fac9743eb6a26c0abe22a1f9e0df9a1281caf2e
Victor Fontenele <victorvitoria11@hotmail.com>
2020-07-28 01:30:48 -0300

    feat: test settings

---
+++ src/__tests__/components/Footer.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+describe('Footer component', () => {
+  it('should be able to test the test', () => {
+    const result = 4 + 2;
+
+    expect(result).toEqual(6);
+  });
+});
